80904bf1eca2c4e90423d0dbb680efc9c24395b2
Julen <platerojulen@gmail.com>
2021-01-17 19:24:49 +0100

    añadiendo interactividad al botón día 11

---
+++ app.js
@@ -0,0 +1,9 @@
+function diadeSemana(){
+    document.getElementById("demo").innerHTML = "Hoy es día 11 de Enero"
+    document.getElementById("demo").style.backgroundColor = "#32312e"
+    document.getElementById("demo").style.border = "white solid"
+}
+
+class dia17Semana extends diadeSemana {
+    diadeSemana = document.getElementById("demo").innerHTML = "Hoy es día 18 de Enero"
+}
